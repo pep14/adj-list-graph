609ab90ac72d686a0920d4545a53be8f8e290f3d
pep14 <peter1uhler@gmail.com>
2026-09-25 09:36:36 +0200

    dsgtdsg

--- graph.py
+++ graph.py
@@ -1,3 +1,36 @@
+from node import Node
+import tkinter as tk
+
+
+NODE_SIZE = 15
+
+
 class Graph:
     def __init__(self):
-        ...
+        self.nodes: dict[str : Node] = {}
+
+    def createNode(self, data: str, x, y):
+        if data not in self.nodes:
+            self.nodes[data] = Node(data, x, y)
+        else:
+            print("%s already exists." % data)
+
+    def createEdge(self, data0: str, data1: str, edgeValue):
+        if  data0 in self.nodes and \
+            data1 in self.nodes:
+            self.nodes[data0].connect(self.nodes[data1], edgeValue)
+
+    def draw(self, canvas: tk.Canvas):
+        for node in self.nodes.values():
+            for edge in node.edges:
+                adj = edge[0]
+
+                ax = (node.x + adj.x) // 2
+                ay = (node.y + adj.y) // 2
+
+                canvas.create_line(node.x, node.y, adj.x, adj.y, fill="#aaaaaa")
+                canvas.create_text(ax, ay, text=edge[1])
+
+        for node in self.nodes.values():
+            canvas.create_oval(node.x-NODE_SIZE, node.y-NODE_SIZE, node.x+NODE_SIZE, node.y+NODE_SIZE, fill="#ffffff")
+            canvas.create_text(node.x, node.y, text=node.data)
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from graph import Graph
-from node import Node
+from tkinter import *
 
 
 if __name__ == "__main__":
-    ...
-    
+    graph = Graph()
+    canvas = Canvas(height=800, width=800)
+    canvas.pack()
+
+    graph.createNode("1", 50, 50)
+    graph.createNode("2", 300, 500)
+    graph.createNode("3", 100, 300)
+    graph.createEdge("1", "2", 50)
+    graph.createEdge("1", "3", 100)
+    graph.draw(canvas)
+
+    canvas.mainloop()
